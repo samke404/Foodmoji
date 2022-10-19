eb8f0b58919f36cd05e6628bf1fc707bcb35e40d
samke404 <nmhlongo880@gmail.com>
2022-10-19 15:35:33 +0200

    Update Models Based On Review

---
+++ Foodmoji_Infastructure/Migrations/20221019133245_Code_Review_Model_Changes.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Foodmoji_Infastructure.Migrations
+{
+    public partial class Code_Review_Model_Changes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "RoleName",
+                table: "Roles",
+                newName: "Name");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Roles",
+                newName: "RoleName");
+        }
+    }
+}
